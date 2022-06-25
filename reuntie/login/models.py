632b79fd25de0b56a42979a0dbe12b0c4b5151c6
Himanshu Dhingra <mfsi.himanshu.d@gmail.com>
2022-06-25 18:48:43 +0530

    changes

---
+++ reuntie/login/models.py
@@ -0,0 +1,80 @@
+from operator import mod
+from xmlrpc.client import Boolean
+from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser
+from .manager import UserManager
+from django.contrib.auth.models import PermissionsMixin
+
+# Create your models here.rue
+
+class User(AbstractBaseUser,PermissionsMixin):
+
+    # username=models.CharField(max_length=20,unique=True,primary_key=True)
+    email=models.EmailField(verbose_name='email',max_length=60,unique=True)
+    # password=models.CharField(max_length=20)
+    first_name=models.CharField(max_length=20)
+    last_name=models.CharField(max_length=20)
+    status=models.CharField(max_length=10,null=True,blank=True)
+    friend=models.ManyToManyField("self")
+    created_at=models.DateTimeField(auto_now_add=True)
+    # is_active=models.BooleanField(default=False,blank=True,null=True)
+    objects=UserManager()
+
+    USERNAME_FIELD= 'email'
+    REQUIRED_FIELDS= []
+
+class Image(models.Model):
+    image=models.ImageField(upload_to="Images")
+    created_at=models.DateTimeField(auto_now_add=True)
+
+
+class Post(models.Model):
+
+    body=models.TextField(null=True,blank=True)
+    image=models.ManyToManyField("Image", related_name="post_image")
+    user=models.ForeignKey('User', on_delete=models.CASCADE)
+    create_at=models.DateTimeField(auto_now_add=True)
+
+class Comments(models.Model):
+    
+    post=models.ForeignKey("Post",on_delete=models.CASCADE)
+    comment_text=models.CharField(max_length=100)
+    user=models.ForeignKey("User",on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+class Likes(models.Model):
+
+    post=models.ForeignKey("Post",on_delete=models.CASCADE)
+    user=models.ForeignKey("User",on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+class Notification(models.Model):
+
+    from_user=models.ForeignKey("User", on_delete=models.CASCADE,related_name="from_user")  
+    username=models.ForeignKey("User", on_delete=models.CASCADE,related_name="username")
+    created_at=models.DateTimeField(auto_now_add=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ reuntie/login/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path,include
+from .import views
+
+urlpatterns = [
+    path('home/',views.home,name="home"),
+    path('signup/',views.signup,name="signup"),
+    path('signin/',views.signin,name="signin"),
+    path('signout/',views.signout,name="signout"),
+    # path('activate/<uidb64>/<token>', views.activate, name='activate'),
+    path('profile/',views.profile,name='users-profile'),
+    path('thanks/',views.thanks,name="thanks"),
+    path('create_post/',views.create_post,name="create_post"),
+    
+
+]
+
